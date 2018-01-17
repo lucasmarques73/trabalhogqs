847da354d4cf263869d880fa83122e9f62a5c85e
Lucas Marques <lucasmarques73@hotmail.com>
2018-01-17 11:04:53 -0200

    Versionando projeto

---
+++ enviar_mensagem.php
@@ -0,0 +1,19 @@
+
+<html>
+ <head>
+  <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <title>Cadastro de Usuário - LUVI</title>
+ </head>
+ <body>
+<div class="container" style="text-align: center;">
+ <div class="control-group span9">
+<h4>Cadastro Efetuado Com Sucesso!</h4>
+
+   <a href="index.html" class="btn btn-success">Novo Usuário</a>
+</div>
+</div>
+ </body>
+</html>
